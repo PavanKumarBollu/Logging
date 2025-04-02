5360ee031d59465dcc9d99e4e7536001ed4fba81
PavanKumarB <bollupavankumar12@gmail.com>
2025-04-03 02:00:39 +0530

    Spring Boot Project to demonstract the Log4j Use in boot Appications

--- 10_SpringBoot_Loggin_Using_Log4j/src/main/java/com/pavan/beans/CloudCourse.java
+++ 10_SpringBoot_Loggin_Using_Log4j/src/main/java/com/pavan/beans/CloudCourse.java
@@ -1,5 +1,25 @@
 package com.pavan.beans;
 
-public class CloudCourse {
+public class CloudCourse implements ICourse {
+	static
+	{
+		System.out.println("CloudCourse .class file is loading.....\n");
+	}
+		
+	public CloudCourse() {
+		System.out.println("CloudCourse Zero Param Constructor got called.....\n");
+	}
+
+	@Override
+	public String courseContent() {
+		System.out.println("CloudCourse Content Method Got Called.....\n");
+		return "1.Docker 2. Kubernates 3.  Azure 4. AWS.....\n";
+	}
+
+	@Override
+	public float price() {
+		
+		return 700;
+	}
 
 }
