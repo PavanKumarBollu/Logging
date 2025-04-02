b65fffb3ac38a3650a16568e5080de38455680b2
PavanKumarB <bollupavankumar12@gmail.com>
2025-04-03 02:16:46 +0530

    Spring Boot Project to demonstract the Log4j Use in boot Appications

--- 10_SpringBoot_Loggin_Using_Log4j/src/main/java/com/pavan/Application.java
+++ 10_SpringBoot_Loggin_Using_Log4j/src/main/java/com/pavan/Application.java
@@ -4,11 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.ImportResource;
 
 import com.pavan.beans.Student;
 
 @SpringBootApplication
-
+@ImportResource(locations = "applicationContext.xml")
 public class Application {
 
 	public static void main(String[] args) {
