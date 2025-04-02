5d2f8700ba44f2d2592f9c9c1547eb7a9110c8f8
PavanKumarB <bollupavankumar12@gmail.com>
2025-04-03 02:00:30 +0530

    Spring Boot Project to demonstract the Log4j Use in boot Appications

--- 10_SpringBoot_Loggin_Using_Log4j/src/main/java/com/pavan/beans/UICourse.java
+++ 10_SpringBoot_Loggin_Using_Log4j/src/main/java/com/pavan/beans/UICourse.java
@@ -1,17 +1,27 @@
 package com.pavan.beans;
 
+
 public class UICourse implements ICourse {
+	static
+	{
+		System.out.println("UICourse .class file is loading.....\n");
+	}
+		
+	public UICourse() {
+		System.out.println("UICourse Zero Param Constructor got called.....\n");
+	}
 
 	@Override
 	public String courseContent() {
-		// TODO Auto-generated method stub
-		return null;
+		System.out.println("UICourse Content Method Got Called.....\n");
+		return "1. HTML 2. CSS 3.  JavaScript 4. React Js.....\n";
 	}
 
 	@Override
 	public float price() {
-		// TODO Auto-generated method stub
-		return 0;
+		
+		return 600;
 	}
+
 
 }
